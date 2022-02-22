34d08ed990ae32f229b03db25608694f31765a94
Dor Refaeli <dor@pepperi.com>
2022-02-22 16:08:35 +0200

    fixed addon name

--- client-side/src/app/app.routes.ts
+++ client-side/src/app/app.routes.ts
@@ -16,11 +16,11 @@ const routes: Routes = [
         path: `settings/:addon_uuid`,
         children: [
             {
-                path: 'todos',
+                path: 'TodoAddon',
                 component: AddonComponent,
             },
             {
-                path: 'todos/:todo_uuid',
+                path: 'TodoAddon/:todo_uuid',
                 component: TodoForm
             }
         ]
